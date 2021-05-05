1a478e7cfd285f080afeace2805f4d70ea32afdb
abubaker <mohammad_abubaker@yahoo.com>
2021-05-05 13:32:59 +0500

    Working...

--- app/src/main/java/org/abubaker/demotexttospeech/MainActivity.kt
+++ app/src/main/java/org/abubaker/demotexttospeech/MainActivity.kt
@@ -2,17 +2,78 @@ package org.abubaker.demotexttospeech
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import org.abubaker.demotexttospeech.databinding.ActivityMainBinding
+import org.abubaker.demotexttospeech.databinding.ContentMainBinding
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     // Binding Object
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bindContent: ContentMainBinding
+
+    // Variable for TextToSpeech
+    private var tts: TextToSpeech? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
+
+        setSupportActionBar(binding.toolbar)
+
+        // Initialize the Text To Speech
+        tts = TextToSpeech(this, this)
+
+        bindContent.btnSpeak.setOnClickListener {
+            if (bindContent.etEnteredText.text.isEmpty()) {
+                Toast.makeText(this@MainActivity, "Enter a text to speak.", Toast.LENGTH_SHORT).show()
+            } else {
+                speakOut(bindContent.etEnteredText.text.toString())
+            }
+        }
+
+    }
+
+    /**
+     * This the TextToSpeech override function
+     *
+     * Called to signal the completion of the TextToSpeech engine initialization.
+     */
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            // set US English as language for tts
+            val result = tts!!.setLanguage(Locale.US)
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "The Language specified is not supported!")
+            }
+
+        } else {
+            Log.e("TTS", "Initialization Failed!")
+        }
+    }
+
+    /**
+     * Here is Destroy function we will stop and shutdown the tts which is initialized above.
+     */
+    public override fun onDestroy() {
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+
+        super.onDestroy()
+    }
+
+    /**
+     * Function is used to speak the text what we pass to it.
+     */
+    private fun speakOut(text: String) {
+        tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
     }
 
 }
