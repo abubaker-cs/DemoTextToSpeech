f8ddab3054866a691af1380cb3c00ee5206b57a8
abubaker <mohammad_abubaker@yahoo.com>
2021-05-05 12:59:58 +0500

    Base setup

--- app/src/main/java/org/abubaker/demotexttospeech/MainActivity.kt
+++ app/src/main/java/org/abubaker/demotexttospeech/MainActivity.kt
@@ -2,10 +2,17 @@ package org.abubaker.demotexttospeech
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.abubaker.demotexttospeech.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // Binding Object
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
     }
+
 }
